d26e19c1daa926b3d14506ca4362fbb9080c0552
suganthiganapathy <suganthi1702.kodehode@gmail.com>
2023-01-24 15:32:39 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react-dom/client";
+
+import Calculator from "./components/Calculator";
+import './styles/classses.css'
+import './styles/pallet.css'
+
+function App() {
+ 
+return(
+<Calculator />
+)
+}
+
+export default App;
